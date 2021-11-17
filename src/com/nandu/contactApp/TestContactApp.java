dcf5ce62aba4962741d30fed0d5ee9a904ca6b4a
Nandini Gnanesh <nandudtc@gmail.com>
2021-11-17 17:27:19 +0530

    main menu implemented for contact app

---
+++ src/com/nandu/contactApp/TestContactApp.java
@@ -0,0 +1,14 @@
+package com.nandu.contactApp;
+
+public class TestContactApp {
+	
+	
+
+	public static void main(String[] args) {
+		
+		ContactApp contactApp = new ContactApp();
+		contactApp.start();
+
+	}
+
+}
